f77adaaa2beb852524facf7586bb94e0740c1d2f
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-04-15 00:16:26 -0300

    realize list(get) http request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 import backgroundImage from './assets/monte.jpg';
 
+import api from './services/api';
+
 import Header from './components/Header';
 
 function App() {
-  const [techs, setTechs] = useState(['React', 'React Native', 'Angular']);
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    api.get('techs').then((res) => {
+      setTechs(res.data);
+    });
+  }, []);
 
   function handleAddTech() {
     setTechs([...techs, `Nova tecnologia ${Date.now()}`]);
@@ -17,14 +25,13 @@ function App() {
       <img src={backgroundImage} width="300" />
       <ul>
         {techs.map((tech) => (
-          <li key={tech}>{tech}</li>
+          <li key={tech.id}>{tech.title}</li>
         ))}
       </ul>
 
       <button type="button" onClick={handleAddTech}>
         Adicionar tecnologia
       </button>
-
     </Header>
   );
 }
